6d9ffeb939fdd02716d6240a24b721c00c3b0a3e
whatacalamity <clamityleaksreal@gmail.com>
2026-01-14 18:25:41 +0000

    Update among us save editor.py

--- among us save editor.py
+++ among us save editor.py
@@ -65,18 +65,46 @@ try:
 
         ###########################################################################
         
-        source = "https://raw.githubusercontent.com/whatacalamity/sus/refs/heads/main/sus.py"
+        source_main = "https://raw.githubusercontent.com/whatacalamity/sus/refs/heads/main/sus.py"
+        source_updater = "https://raw.githubusercontent.com/whatacalamity/sus/refs/heads/main/among us save editor.py"
         
         ###########################################################################
 
-        
+
         try:
-            response = requests.get(source)
+            response = requests.get(source_updater)
         except:
-            print(f"{hl('Could not connect to server. Check your internet connection.', 255, 120, 0)}")
-            input("| [ENTER] to skip update and launch | ")
+            print(f"{hl('Could not connect to server. Check your internet connection. (among us save editor.py)', 255, 120, 0)}")
+            print(f"{hl('Skipping launcher update...', 255, 120, 0)}")
+        else:
+            if response.status_code == 200:
+                with open(__file__, "rb") as file:
+                    a = file.read()
+                    text = response.content
+                    if a != text:
+                        print(hl('Update found! (among us save editor.py)', 255, 200, 0))
+                        print(hl("Select action:\n'source' - Show source (github)\n'install' - Install update\n'skip' - Continue without updating", 255, 255, 255))
+                        while True:
+                            action = input("> ").lower()
+                            if action == "source":
+                                webbrowser.open_new_tab(source_updater)
+                            elif action == "install":
+                                print(f"{hl('Updating...', 255, 200, 0)}")
+                                with open(__file__, "wb") as file:
+                                    file.write(text)
+                                print(hl('All up to date! (among us save editor.py)', 0, 255, 0))
+                            os.system(f"{__file__} 1")
+                    else:
+                        print(hl('All up to date! (among us save editor.py)', 0, 255, 0))
+            else:
+                print(f"{hl('Could not connect to server. Check your internet connection.', 255, 120, 0)}")
+                print(f"{hl('Skipping launcher update...', 255, 120, 0)}")
+        try:
+            response = requests.get(source_main)
+        except:
+            print(f"{hl('Could not connect to server. Check your internet connection. (sus.py)', 255, 120, 0)}")
+            print(f"{hl('Skipping editor update...', 255, 120, 0)}")
             os.system(f"{root}\\sus.py 1")
-            input("| [ENTER] to close program | ")
         else:
             if response.status_code == 200:
                 if not os.path.exists(f"{root}\\sus.py"):
@@ -85,25 +113,31 @@ try:
                     a = file.read()
                     text = response.content
                     if a != text:
-                        print(hl('Update found!', 255, 200, 0))
+                        print(hl('Update found! (sus.py)', 255, 200, 0))
                         print(hl("Select action:\n'source' - Show source (github)\n'install' - Install update and launch\n'skip' - Launch without updating", 255, 255, 255))
                         while True:
                             action = input("> ").lower()
                             if action == "source":
-                                webbrowser.open_new_tab(source)
+                                webbrowser.open_new_tab(source_main)
                             elif action == "install":
                                 print(f"{hl('Updating...', 255, 200, 0)}")
                                 with open(f"{root}\\sus.py", "wb") as file:
                                     file.write(text)
-                                print(hl('All up to date!', 0, 255, 0))
+                                print(hl('All up to date! (sus.py)', 0, 255, 0))
                             os.system(f"{root}\\sus.py 1")
                     else:
-                        print(hl('All up to date!', 0, 255, 0))
+                        print(hl('All up to date! (sus.py)', 0, 255, 0))
                         os.system(f"{root}\\sus.py 1")
             else:
                 print(f"{hl('Could not connect to server. Check your internet connection.', 255, 120, 0)}")
+                input("| [ENTER] to launch without updating | ")
+                os.system(f"{root}\\sus.py 1")
                 input("| [ENTER] to close program | ")
 except Exception as e:
-    print(f"Error opening the editor. You should probably report this\n{e}")
+    print(f"Error opening the launcher. You should probably report this\n{e}")
+    try:
+        input("| [ENTER] to launch without updating | ")
+        os.system(f"{root}\\sus.py 1")
+    except Exception as e:
+        print(f"Error opening the editor. You should probably report this\n{e}")
     input("| [ENTER] to close program | ")
-
