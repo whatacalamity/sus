0f865d4843f1c6d67ec047690979d0fdd7066664
whatacalamity <clamityleaksreal@gmail.com>
2026-05-11 17:58:15 +0100

    Update sus.py

--- sus.py
+++ sus.py
@@ -1,4 +1,5 @@
 VERSION = "0.0"
+print("Heads up! The editor is being rewritten, and this version is not updated yet, expect a lot of bugs.")
 __import__("time").sleep(0.5)
 __import__("os").system("cls")
 try:
